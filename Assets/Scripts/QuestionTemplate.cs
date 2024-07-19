eb6d51dcd53fc1e839b79cfe60e4c4cd5fbd2c17
Jace Weerawardena <jasonweerawardena@gmail.com>
2024-07-19 21:18:04 +0100

    completed scriptable object with getters

--- Assets/Scripts/QuestionTemplate.cs
+++ Assets/Scripts/QuestionTemplate.cs
@@ -6,9 +6,20 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Question", fileName = "New Question")]
 public class QuestionTemplate : ScriptableObject
 {
-    [TextArea(2, 6)]
-    [SerializeField] string question = "Enter new question here";
+    [TextArea(2, 6)] [SerializeField] private string question = "Enter new question here";
+    [TextArea(2, 6)] [SerializeField] private string[] answers = new string[4];
+    [SerializeField] private int correctAnswerIndex = 0;
 
+    public string GetQuestion(){
+        return question;
+    }
 
+    public string GetAnswer(int index){
+        return answers[index];
+    }
+
+    public int GetCorrectAnswerIndex(){
+        return correctAnswerIndex;
+    }
 
 }
