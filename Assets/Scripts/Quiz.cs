a5b84e96eb1462ae54d1ba926cc856fb51be29c5
Jace Weerawardena <jasonweerawardena@gmail.com>
2024-07-22 22:07:22 +0100

    correct and incorrect answer response

--- Assets/Scripts/Quiz.cs
+++ Assets/Scripts/Quiz.cs
@@ -2,15 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class Quiz : MonoBehaviour
 {
 
+    //QuestionScriptableObject
     [SerializeField] TextMeshProUGUI questionText;
     [SerializeField] QuestionScriptableObject question;
     [SerializeField] GameObject[] answerButtons = new GameObject[4];
+    int correctAnswerIndex;
+
+    //Sprites
+    [SerializeField] Sprite defaultAnswerButtonSprite;
+    [SerializeField] Sprite correctAnswerButtonSprite;
+
     private void Start(){
+
         questionText.text = question.GetQuestion();
+        correctAnswerIndex = question.GetCorrectAnswerIndex();
 
         //goes through all button objects, finds the first textmeshpro child component and prints the question answer at that array index on that button
         for (int i = 0; i < answerButtons.Length; i++){
@@ -18,5 +28,19 @@ public class Quiz : MonoBehaviour
             buttonText.text = question.GetAnswer(i);
         }
 
+    }
+
+    public void OnAnswerSelected(int index){
+        if (index == correctAnswerIndex){
+            questionText.text = "Correct!";
+            Image buttonImage = answerButtons[index].GetComponent<Image>();
+            buttonImage.sprite = correctAnswerButtonSprite;
+        }
+        else{
+            string questionBoxRevalingcorrectAnswer = "Incorrect, correct answer was: " + question.GetAnswer(question.GetCorrectAnswerIndex());
+            questionText.text = questionBoxRevalingcorrectAnswer;
+            Image buttonImage = answerButtons[correctAnswerIndex].GetComponent<Image>();
+            buttonImage.sprite = correctAnswerButtonSprite;
+        }
     }
 }
